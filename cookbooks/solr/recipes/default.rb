22c750b16b367ea213fa8cfdf520b210e7b89ec3
Anton Butovich <abutovich@gmail.com>
2014-01-16 14:40:36 +0200

    Support prepaired collections

--- cookbooks/solr/recipes/default.rb
+++ cookbooks/solr/recipes/default.rb
@@ -47,43 +47,105 @@ execute "copy sorl.war" do
   creates "#{node["solr"]["path"]}/webapps/solr.war"
 end
 
-#Create collections
-node["solr"]["collection"].each do |collection|
-  directory "#{node["solr"]["path"]}/cores/#{collection}/conf/" do
-    owner node["tomcat"]["user"]
-    group node["tomcat"]["group"]
-    mode 00755
-    recursive true
-    action :create
+#Populate collections
+if ( node["solr"]["collection"][0].start_with?('http:','https:','ftp:','file:'))
+  #if collection uri
+  require 'uri'
+  collection_uri = node['solr']['collection']
+  collection_uri.each do |collection|
+    uri = URI.parse(collection)
+    file_name = File.basename(uri.path)
+    ext_name = File.extname(file_name)
+    target_file = "/tmp/#{file_name}"
+
+    #check is extention zip|tar.gz
+    if ( ! %w{.zip .gz }.include?(ext_name))
+      fail "not supported"
+    end
+    #download to target_file
+    if ( collection.start_with?('http','ftp'))
+      remote_file "#{target_file}" do
+        source collection
+      end
+    elsif ( collection.start_with?('file'))
+      target_file = URI.parse(collection).path
+    end
+
+    file_name = File.basename(target_file)
+    
+    #create solr/cores dir
+    directory "#{node["solr"]["path"]}/cores" do
+      owner "#{node["tomcat"]["user"]}"
+      group "#{node["tomcat"]["user"]}"
+      mode "00755"
+      action :create
+    end
+
+    #extract collection archive
+    case ext_name
+    when ".gz"
+      bash "unpack collection #{target_file}" do
+        user "root"
+        code <<-EOH
+        tar -xzvf #{target_file} -C #{node["solr"]["path"]}/cores/
+        chown -R #{node["tomcat"]["user"]}:#{node["tomcat"]["user"]} #{node["solr"]["path"]}/cores
+        chmod -R 755 #{node["solr"]["path"]}/cores
+        EOH
+      end
+    when ".zip"
+      package "zip" do
+        action :install
+      end
+      bash "unpack collection #{target_file}" do
+        user "root"
+        code <<-EOH
+        unzip -o #{target_file} -d #{node["solr"]["path"]}/cores/
+        chown -R #{node["tomcat"]["user"]}:#{node["tomcat"]["user"]} #{node["solr"]["path"]}/cores
+        chmod -R 755 #{node["solr"]["path"]}/cores
+        EOH
+      end
+    end
+  end
+else
+  #If collection not uri
+  #Create collections
+  node["solr"]["collection"].each do |collection|
+    directory "#{node["solr"]["path"]}/cores/#{collection}/conf/" do
+      owner node["tomcat"]["user"]
+      group node["tomcat"]["group"]
+      mode 00755
+      recursive true
+      action :create
+    end
+
+    template "#{node["solr"]["path"]}/cores/#{collection}/conf/schema.xml" do
+      owner node["tomcat"]["user"]
+      group node["tomcat"]["group"]
+      source "schema.xml.erb"
+      variables({
+        :collection => "#{collection}"
+      })
+    end
+
+    template "#{node["solr"]["path"]}/cores/#{collection}/conf/solrconfig.xml" do
+      owner node["tomcat"]["user"]
+      group node["tomcat"]["group"]
+      source "solrconfig.xml.erb"
+      variables({
+        :collection => "#{collection}"
+      })
+    end
   end
 
-  template "#{node["solr"]["path"]}/cores/#{collection}/conf/schema.xml" do
+  template "#{node["solr"]["path"]}/cores/solr.xml" do
     owner node["tomcat"]["user"]
     group node["tomcat"]["group"]
-    source "schema.xml.erb"
+    source "solr.xml.erb"
     variables({
-      :collection => "#{collection}"
+      :zooip => node["solr"]["zookeeper"]["host"],
+      :zooport => node["solr"]["zookeeper"]["port"]
     })
   end
-
-  template "#{node["solr"]["path"]}/cores/#{collection}/conf/solrconfig.xml" do
-    owner node["tomcat"]["user"]
-    group node["tomcat"]["group"]
-    source "solrconfig.xml.erb"
-    variables({
-      :collection => "#{collection}"
-    })
-  end
-end
-
-template "#{node["solr"]["path"]}/cores/solr.xml" do
-  owner node["tomcat"]["user"]
-  group node["tomcat"]["group"]
-  source "solr.xml.erb"
-  variables({
-    :zooip => node["solr"]["zookeeper"]["host"],
-    :zooport => node["solr"]["zookeeper"]["port"]
-  })
 end
 
 if ( ! node["solr"]["zookeeper"]["host"].empty? )
