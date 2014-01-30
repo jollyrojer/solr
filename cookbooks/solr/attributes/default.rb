f0754e7a4068b740a9a6c61aec8fac64def4eb68
Anton Butovich <abutovich@gmail.com>
2014-01-30 18:17:14 +0200

    Split solr install and create collections

--- cookbooks/solr/attributes/default.rb
+++ cookbooks/solr/attributes/default.rb
@@ -4,10 +4,18 @@ default["solr"]["version"] = "4.6.0"
 default["solr"]["path"] = "/opt/solr"
 default["solr"]["slf4j"]["url"] = "http://www.slf4j.org/dist"
 default["solr"]["slf4j"]["version"] = "1.6.6"
-default["solr"]["collection"] = ["one","two"]
-default["solr"]["zookeeper"]["nodes"] = ""
+default["solr"]["collection"] = []
 default["solr"]["zookeeper"]["port"] = "2181"
-default["solr"]["port"]=""
+default["solr"]["zookeeper"]["timeout"] = "10000" 
+default["solr"]["zookeeper"]["hosts"] = []
+
+zoo_nodes = []
+node["solr"]["zookeeper"]["hosts"].each do |zoonode|
+    zoo_nodes << "#{zoonode}:#{node["solr"]["zookeeper"]["port"]}"
+end
+set["solr"]["zookeeper"]["nodes"] = zoo_nodes.join(",")
+
+default["solr"]["port"]="8983"
 default["solr"]["hostcontext"]="solr"
 default["tomcat"]["base_version"] = 6
 case node['platform']
