407bf6e5f9d062cb34e06391cc71e354d86d1412
Artem Ivantsov <loky@Artems-MacBook-Pro.local>
2014-04-23 12:25:16 +0300

    fix solr_download lwpr

--- cookbooks/solr/recipes/default.rb
+++ cookbooks/solr/recipes/default.rb
@@ -8,9 +8,9 @@ require "pathname"
 # Extract war file from solr archive
 solr_url = "#{node["solr"]["url"]}#{node["solr"]["version"]}/solr-#{node["solr"]["version"]}.tgz"
 solr_download "#{node["solr"]["version"]}"  do
-  solr_url solr_url 2
+  solr_url solr_url
   solr_version  "#{node["solr"]["version"]}"
-  solr_checksum "#{node["solr"]["checksum"]fetch(node.solr.version, nil)}"
+  solr_checksum "#{node["solr"]["checksum"].fetch(node.solr.version, nil)}"
   action :download_extract
   retries 3
 end
@@ -24,7 +24,7 @@ ruby_block "get logging libs" do
     libs=(Dir.entries(folder).select {|f| !File.directory? f}).map {|f| "file://" + File.join(folder, f)}
     node.set["solr"]["lib_uri"] = libs
   end
- # subscribes :create, resources(:solr_download => "#{node["solr"]["version"]}")
+  subscribes :create, resources(:solr_download => "#{node["solr"]["version"]}")
 end
 
 #Copy sorl.war to webapps
