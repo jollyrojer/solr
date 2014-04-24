0b0255eec3ca194bf4333a8254734d924a3c130c
Artem Ivantsov <loky@Artems-MacBook-Pro.local>
2014-04-24 12:28:46 +0300

    fix download lwpr

--- cookbooks/solr/providers/download.rb
+++ cookbooks/solr/providers/download.rb
@@ -8,6 +8,6 @@ end
           
 execute "extract solr_src" do
   command "tar -xzvf /tmp/solr-#{@new_resource.ver}.tgz -C /tmp"
-  creates "/tmp/solr-#{@new_resource.ver"
+  creates "/tmp/solr-#{@new_resource.ver}"
   end
 end
