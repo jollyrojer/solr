9ff67462051644a7bd72e2fb3d303482abc53a4d
Artem Ivantsov <loky@Artems-MacBook-Pro.local>
2014-04-24 16:20:05 +0300

    fix download lwrp

--- cookbooks/solr/providers/download.rb
+++ cookbooks/solr/providers/download.rb
@@ -1,13 +1,16 @@
 action :download_extract do
-remote_file "solr_src" do
-  path "/tmp/solr-#{@new_resource.ver}"
-  source "#{@new_resource.url}"
-  checksum "#{@new_resource.checksum}"
-  action :create_if_missing
-end
+  version = new_resource.ver
+  url = new_resource.url
+  checksum = new_resource.checksum
+  remote_file "solr_src" do
+    path "/tmp/solr-#{version}.tgz"
+    source "#{url}"
+    checksum "#{checksum}"
+    action :create_if_missing
+  end
           
-execute "extract solr_src" do
-  command "tar -xzvf /tmp/solr-#{@new_resource.ver}.tgz -C /tmp"
-  creates "/tmp/solr-#{@new_resource.ver}"
+  execute "extract solr_src" do
+    command "tar -xzvf /tmp/solr-#{version}.tgz -C /tmp"
+    creates "/tmp/solr-#{version}"
   end
 end
--- cookbooks/solr/resources/download.rb
+++ cookbooks/solr/resources/download.rb
@@ -2,3 +2,9 @@ actions :download_extract
 attribute :url, :kind_of => String
 attribute :ver,  :default => "4.4.0", :kind_of => String, :name_attribute => true
 attribute :checksum,  :kind_of => String
+
+def initialize(*args)
+  super
+  @resource_name = :solr_download
+  @action = :download_extract
+end
