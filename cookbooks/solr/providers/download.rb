12317e505efcba54e1d5f47f65ca57fc2bb1b160
Artem Ivantsov <loky@Artems-MacBook-Pro.local>
2014-04-24 11:37:57 +0300

    fix download lwpr

--- cookbooks/solr/providers/download.rb
+++ cookbooks/solr/providers/download.rb
@@ -1,13 +1,13 @@
 action :download_extract do
 remote_file "solr_src" do
-  path "/tmp/solr-#{@new_resource.version}"
+  path "/tmp/solr-#{@new_resource.ver}"
   source "#{@new_resource.url}"
   checksum "#{@new_resource.checksum}"
   action :create_if_missing
 end
           
 execute "extract solr_src" do
-  command "tar -xzvf /tmp/solr-#{@new_resource.version}.tgz -C /tmp"
-  creates "/tmp/solr-#{@new_resource.version}"
+  command "tar -xzvf /tmp/solr-#{@new_resource.ver}.tgz -C /tmp"
+  creates "/tmp/solr-#{@new_resource.ver"
   end
 end
--- cookbooks/solr/resources/download.rb
+++ cookbooks/solr/resources/download.rb
@@ -1,4 +1,4 @@
 actions :download_extract
 attribute :url, :kind_of => String
-attribute :version,  :default => "4.4.0", :kind_of => String, :name_attribute => true
+attribute :ver,  :default => "4.4.0", :kind_of => String, :name_attribute => true
 attribute :checksum,  :kind_of => String
