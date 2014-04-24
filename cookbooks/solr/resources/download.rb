de3390ae0145d55ef468bbeb761565eb9ed42c8e
Artem Ivantsov <loky@Artems-MacBook-Pro.local>
2014-04-24 06:40:51 +0300

    fix download lwpr

--- cookbooks/solr/resources/download.rb
+++ cookbooks/solr/resources/download.rb
@@ -1,4 +1,4 @@
 actions :download_extract
 attribute :url, :kind_of => String
-attribute :version,  :kind_of => String
+attribute :version,  :default => "4.4.0"
 attribute :checksum,  :kind_of => String
